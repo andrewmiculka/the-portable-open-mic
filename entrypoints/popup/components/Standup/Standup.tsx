c1d2626b57251b2262c7d7b72c50cbd8cea9ca05
andrewmiculka <andrewmiculka@gmail.com>
2024-09-25 23:27:38 -0500

    volume controls

--- entrypoints/popup/components/Standup/Standup.tsx
+++ entrypoints/popup/components/Standup/Standup.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import talkingStandupSrc from '~/assets/images/Standup/animated.gif';
 import staticStandupSrc from '~/assets/images/Standup/static.png';
 import microphoneSrc from '~/assets/images/Standup/mic.png';
@@ -7,15 +7,19 @@ import './Standup.css';
 
 const PITCH_MIN = 0.2;
 const PITCH_MAX = 2;
+const VOLUME_ON = 'volume_up';
+const VOLUME_OFF = 'volume_off';
 
 const Standup = () => {
+    const audioRef = useRef<HTMLAudioElement>();
     const [isTalking, setIsTalking] = useState(false);
-    const [joke, setJoke] = useState('I took your mom on a date to the Westminster Dog Show. . . She won.');
-
-    const synth = new Animalese();
+    const [isMuted, setIsMuted] = useState(false);
+    const [joke, setJoke] = useState('I took your mom on a date to the Westminster Dog Show. . . . . . . She won.');
 
     const generateAudio = async () => {
+        const synth = new Animalese();
         const audio = new Audio();
+        audioRef.current = audio;
         await synth.init();
         audio.src = synth.textToAnimalese(joke, false, Math.random() * (PITCH_MAX - PITCH_MIN) + PITCH_MIN).dataURI;
         audio.onplaying = () => {
@@ -28,11 +32,22 @@ const Standup = () => {
     }
 
     useEffect(() => {
+        if (audioRef?.current) {
+            audioRef.current.muted = isMuted;
+        }
+    }, [isMuted]);
+
+    useEffect(() => {
         generateAudio();
     }, [joke]);
 
     return (
         <section className="stage">
+            <button className='volume-button' onClick={() => setIsMuted(!isMuted)}>
+                <span className="material-symbols-outlined">
+                    { isMuted ? VOLUME_OFF : VOLUME_ON }
+                </span>
+            </button>
             <img className='standup' src={isTalking ? talkingStandupSrc : staticStandupSrc } />
             <img className='microphone' src={microphoneSrc} />
         </section>
